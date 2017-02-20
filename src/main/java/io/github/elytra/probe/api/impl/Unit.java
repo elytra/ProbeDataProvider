61481035e61ca60c1006cc5d1f2c9406ba810db0
Falkreon <falkreon@gmail.com>
2017-02-19 23:15:16 -0600

    The sun releases about 12,140,000 hellajoules in a year (~0.3848XJ/sec)
    formatSI now supports fractional and hella SI prefixes.

--- src/main/java/io/github/elytra/probe/api/impl/Unit.java
+++ src/main/java/io/github/elytra/probe/api/impl/Unit.java
@@ -36,6 +36,7 @@ public class Unit implements IUnit {
 
 	/* Convenience Methods */
 	
+	private static final double HELLA = 1_000_000_000_000_000_000_000_000_000D;
 	private static final double YOTTA = 1_000_000_000_000_000_000_000_000D;
 	private static final double ZETTA = 1_000_000_000_000_000_000_000D;
 	private static final double EXA   = 1_000_000_000_000_000_000D;
@@ -44,6 +45,10 @@ public class Unit implements IUnit {
 	private static final double GIGA  = 1_000_000_000D;
 	private static final double MEGA  = 1_000_000D;
 	private static final double KILO  = 1_000D;
+	private static final double MILLI = 1/1_000D;
+	private static final double MICRO = 1/1_000_000D;
+	private static final double NANO  = 1/1_000_000_000D;
+	private static final double PICO  = 1/1_000_000_000_000D;
 	private static final NumberFormat FORMAT = NumberFormat.getNumberInstance();
 	static {
 		FORMAT.setMinimumFractionDigits(0);
@@ -51,7 +56,9 @@ public class Unit implements IUnit {
 	}
 	
 	public static String formatSI(double d, IUnit u) {
-		if (d>YOTTA) {
+		if (d>HELLA) {
+			return FORMAT.format(d/HELLA)+" X"+u.getAbbreviation();
+		} else if (d>YOTTA) {
 			return FORMAT.format(d/YOTTA)+" Y"+u.getAbbreviation();
 		} else if (d>ZETTA) {
 			return FORMAT.format(d/ZETTA)+" Z"+u.getAbbreviation();
@@ -67,9 +74,18 @@ public class Unit implements IUnit {
 			return FORMAT.format(d/MEGA)+" M"+u.getAbbreviation();
 		} else if (d>KILO) {
 			return FORMAT.format(d/KILO)+" k"+u.getAbbreviation();
+			
+		//if we ever added femto/atto/zepto/yocto they'd go here
+		//dividing by the reciprocal down there should totally work. It's not the most efficient way, but it's consistent.
+			
+		} else if (d<NANO) {
+			return FORMAT.format(d/PICO)+" p"+u.getAbbreviation();
+		} else if (d<MICRO) {
+			return FORMAT.format(d/NANO)+" n"+u.getAbbreviation();
+		} else if (d<MILLI) {
+			return FORMAT.format(d/MICRO)+" µ"+u.getAbbreviation();
 		} else if (d<1.0) {
-			//TODO: Perhaps descend down the milli/micro/nano/pico ladder
-			return FORMAT.format(d)+" "+u.getAbbreviation();
+			return FORMAT.format(d/MILLI)+" m"+u.getAbbreviation();
 		} else {
 			return FORMAT.format(d)+" "+u.getAbbreviation();
 		}
